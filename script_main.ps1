eea2a9fb0384dce2d4f84497ca76cc396e8bb65d
Khurram <khurramworkspace@gmail.com>
2022-07-08 22:26:50 +0200

    Adding the main file
    Importing the parameters.

---
+++ script_main.ps1
@@ -0,0 +1,9 @@
+﻿
+#######Step 1###########
+#######Importing the credentials from a file#################
+
+Set-Variable -Name "counter" 
+[string]$user= (Get-Content -Path .\paramimport.txt -TotalCount 6)[-5];
+[string]$pass= (Get-Content -Path .\paramimport.txt -TotalCount 6)[-3];
+[string]$tenant1= (Get-Content -Path .\paramimport.txt -TotalCount 6)[-1];
+
