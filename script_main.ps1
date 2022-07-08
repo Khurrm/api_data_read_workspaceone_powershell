b45fc372b256a6387be90481afbbca94888b35ab
Khurram <khurramworkspace@gmail.com>
2022-07-08 22:39:32 +0200

    Base 64 encoding for basic authentication

--- script_main.ps1
+++ script_main.ps1
@@ -7,3 +7,10 @@ Set-Variable -Name "counter"
 [string]$pass= (Get-Content -Path .\paramimport.txt -TotalCount 6)[-3];
 [string]$tenant1= (Get-Content -Path .\paramimport.txt -TotalCount 6)[-1];
 
+#Reference Article 1
+#https://mixedanalytics.com/knowledge-base/api-connector-encode-credentials-to-base-64/
+
+$pair = "${user}:${pass}" 
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
+$base64 = [Convert]::ToBase64String($bytes)
+
